619f999b2731f7d30dc17836eb17b440aecdef27
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-06-21 23:03:39 -0500

    Increment functionality completed

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 //change value setted on h2 tag
 //document.getElementById("count-el").innerText=5;
-/*TO READ: WHAT IS DOCUMENT? */
+/*TO READ: WHAT IS DOCUMENT OBJECT? */
 
-let counter = 0; //Let count be 0 #MAGIC
+/* FIRST TO DO
+1. Initialize the count as 0
+2. Listen for clicks on the increment button (onclick)
+3. Increment the count variable when the button is clicked
+4. Change the count-el in the HTML to reflect the new count */
 
-console.log(counter);
+//Access to the element with the id described
+let counterEl = document.getElementById("count-el");
+let counter = 0;
+
+//Making the function to the onclick event
+function increment(){
+    counter++;
+    //Set counting to the element on the html tag
+    counterEl.innerText = counter;
+}
