928d5f21028abcf9eb44af36f9829ca1d95cd4e3
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-06-22 00:54:27 -0500

    Changing innerText for textContent

--- index.js
+++ index.js
@@ -35,9 +35,33 @@ function save(){
     1. Create a variable that contains both the count and the dash separator
     2. Render the variable in the saveEl using innerText
     IMPORTANT: Make sure to not delete the existing content of the paragraph */
+    /*What is the Difference Between textContents, innerText, and innerHtml?
+    - textContent is all text contained by an element and all its children that are for formatting purposes only.
+    - innerText returns all text contained by an element and all its child elements.
+    - innerHtml returns all text, including html tags, that is contained by an element.*/
+    /*
+    1. The innerHTML property sets and returns the content of an element with new HTML content.
+    console.log(document.getElementById('blog-test').innerHTML);
+    This element is <strong>strong</strong> and   has some super fun <code>code</code>!
+
+    2. The innerText property returns the content of all elements, except for <script> and <style> elements.
+        console.log(document.getElementById('blog-test').innerText);
+        This element is strong and has some super fun code!
+
+    3. The textContent property returns the raw content with styling inside of all elements, but excludes the HTML element tags. This includes <script> and <style> elements, whitespace, line breaks, and carriage returns.
+        console.log(document.getElementById('blog-test').textContent);
+        This element is strong and   has some super fun code!
+
+    Note: Unlike innerHTML, textContent has better performance because its value is not parsed as HTML. 
+    For that reason, using textContent can also prevent Cross-Site Scripting (XSS) attacks. 
+    Unlike innerText, textContent isn't aware of CSS styling and will not trigger a reflow. 
+    All Node objects have textContent, whereas only HTMLElement objects have innerText. */
+
     savePassengers.innerText = "Previous passengers: ";
     let strIncrementos = incrementos.join(" - "); 
-    savePassengers.innerText += " " +strIncrementos;
+    savePassengers.textContent += " " +strIncrementos;
+    counterEl.textContent = 0;
+    counter = 0;
     
     
 }
