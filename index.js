707b9a01a994e293b506cd0a343892c63ea480e4
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-06-21 21:51:37 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+//change value setted on h2 tag
+//document.getElementById("count-el").innerText=5;
+/*TO READ: WHAT IS DOCUMENT? */
+
+let counter = 0; //Let count be 0 #MAGIC
+
+console.log(counter);
